f59a0282ce4a0dcf8559866db78990f13587e334
Tim Potter <tpot@hpe.com>
2017-05-25 14:47:39 +1000

    Create 4-node static etcd cluster

--- recipes/install_docker.rb
+++ recipes/install_docker.rb
@@ -1,4 +1,5 @@
 package "docker.io"
+package "etcd-client"
 
 docker_image "debian" do
   tag "sid"
@@ -23,8 +24,37 @@ docker_image "debian-etcd" do
   action :build_if_missing
 end
 
-docker_container "node1" do
-  repo "debian-etcd"
-  command "/usr/bin/etcd"
-  action :run_if_missing
+# Static cluster
+
+docker_network "etcd-network" do
+  subnet "192.168.10.0/24"
+  gateway "192.168.10.1"
+  action :create
+end
+
+initial_cluster = ""
+
+[10,11,12,13].each do |n|
+  initial_cluster << "node#{n}=http://192.168.10.#{n}:2380,"
+end
+
+[10,11,12,13].each do |n|
+
+  node_ip = "192.168.10.#{n}"
+
+  docker_container "node#{n}" do
+    repo "debian-etcd"
+    command "/usr/bin/etcd --name node#{n} " \
+          "--initial-advertise-peer-urls http://#{node_ip}:2380 " \
+          "--listen-peer-urls http://#{node_ip}:2380 " \
+          "--listen-client-urls http://#{node_ip}:2379 " \
+          "--advertise-client-urls http://#{node_ip}:2379 " \
+          "--initial-cluster-token etcd-cluster " \
+          "--initial-cluster #{initial_cluster} " \
+          "--initial-cluster-state new"
+    network_mode "etcd-network"
+    ip_address node_ip
+    action :run_if_missing
+  end
+  
 end
